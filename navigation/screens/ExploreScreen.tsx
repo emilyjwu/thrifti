208bc69982b30f82f8e23a5b6eefc1dfb53ee4b8
lgastaldi3 <lgastaldi3@gatech.edu>
2024-03-26 00:31:06 -0400

    Image Retrieval Fixes
    - made image retrieval code easier to read + commented
    - readded 'users' to database to help with finding people in explore page

--- navigation/screens/ExploreScreen.tsx
+++ navigation/screens/ExploreScreen.tsx
@@ -11,59 +11,73 @@ interface ExploreScreenProps {
 
 const ExploreScreen: React.FC<ExploreScreenProps> = ({ navigation }) => {
   const [imgURLs, setImgURLs] = useState([]);
-  const [imgRefs, setImgRefs] = useState([]);
-  const [binID, setBinID] = useState("");
   const uid = useContext(AuthContext).userAuth.uid;
 
+  // retrives the ID of all of a user's bins as a [] of binIDs
+  const fetchBins = async (userID: string) => {
+    try {
+      const binQuerySnapshot = await getDocs(
+        query(
+          collection(firestore, "bins"),
+          where("userID", "==", uid),
+          limit(1)
+        )
+      );
+      const binIDs = binQuerySnapshot.docs.map((doc) => doc.id);
+      console.log("Bins: ", binIDs);
+      return binIDs;
+    } catch (error) {
+      console.error("Error retrieving data:", error);
+      return [];
+    }
+  };
+
+  // accesses items for a given bin and returns the image URLs as a [] of URLs
+  const fetchURLs = async (binID: string) => {
+    if (!binID) {
+      return [];
+    }
+    try {
+      const itemQuerySnapshot = await getDocs(
+        query(collection(firestore, "items"), where("binID", "==", binID))
+      );
+      const imgRefs = itemQuerySnapshot.docs.map((doc) => {
+        return ref(storage, "/" + binID + "/" + doc.id);
+      });
+      const urls = await Promise.all(
+        imgRefs.map(async (ref) => {
+          const downloadURL = await getDownloadURL(ref);
+          return downloadURL;
+        })
+      );
+      console.log("URLs: ", urls);
+      return urls;
+    } catch (error) {
+      console.error("Problem retrieving URLs:", error);
+      return [];
+    }
+  };
+
   useEffect(() => {
-    const fetchStoragePath = async () => {
-      try {
-        const binQuerySnapshot = await getDocs(
-          query(
-            collection(firestore, "bins"),
-            where("userID", "==", uid),
-            limit(1)
-          )
-        );
-        console.log("RETRIEVING BIN FOR EXPLORE");
-        const binIDs = binQuerySnapshot.docs.map((doc) => doc.id);
-        console.log(binIDs);
-        setBinID(binIDs[0]);
+    const the = async () => {
+      // these generally should be called in series with await
+      // unless you already have a bin ID you want to get image URLs for
+      // *** uid is temporary to retrieve your own bins
+      const bins = await fetchBins(uid);
 
-        const itemQuerySnapshot = await getDocs(
-          query(collection(firestore, "items"), where("binID", "==", binIDs[0]))
-        );
-        console.log("RETRIEVING ITEMS FOR EXPLORE");
-        const imgRefs = itemQuerySnapshot.docs.map((doc) => {
-          console.log("an item");
-          return ref(storage, "/" + binIDs[0] + "/" + doc.id);
-        });
-        setImgRefs(imgRefs);
+      // this will likely be called for each bin that will be displayed
+      // on the screen
+      /* TODO: figure out how to store image links for various bins instead of just 1
+          to show multiple bins on explore page */
+      // bins[0] will retrieve a random bin of yours bc the queries do not order items
+      const urls = await fetchURLs(bins[0]);
 
-        fetchBinImages(imgRefs);
-      } catch (error) {
-        console.error("Error retrieving data:", error);
-      }
-    };
-
-    const fetchBinImages = async (imgRefs) => {
-      try {
-        const urls = await Promise.all(
-          imgRefs.map(async (ref) => {
-            const downloadURL = await getDownloadURL(ref);
-            return downloadURL;
-          })
-        );
-        console.log("Got urls");
-        console.log(urls);
-        setImgURLs(urls);
-      } catch (error) {
-        console.error("Problem retrieving URLs:", error);
-      }
+      // this just sets the images them to render
+      setImgURLs(urls);
     };
 
     // Call fetchStoragePath to start the data fetching process
-    fetchStoragePath();
+    the();
   }, []);
 
   return (
