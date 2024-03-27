f3d73f09ad6e7423085f42169b9d8117216787a8
lgastaldi3 <lgastaldi3@gatech.edu>
2024-03-27 15:35:12 -0400

    Message navigate fixed

--- navigation/Requests/RequestListing.tsx
+++ navigation/Requests/RequestListing.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
+import * as React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { NavigationProp } from "@react-navigation/native";
 
 interface RequestListingProps {
   navigation: NavigationProp<any>;
@@ -9,14 +9,15 @@ interface RequestListingProps {
 const RequestListing: React.FC<RequestListingProps> = ({ navigation }) => {
   // Sample data for demonstration
   const requestDetails = {
-    name: 'Liam Neeson',
-    description: 'I don’t know who you are. I don’t know what you want. If you are looking for ransom I can tell you I don’t have money, but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you let my daughter go now that’ll be the end of it. I will not look for you, I will not pursue you, but if you don’t, I will look for you, I will find you and I will kill you.',
+    name: "Liam Neeson",
+    description:
+      "I don’t know who you are. I don’t know what you want. If you are looking for ransom I can tell you I don’t have money, but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you let my daughter go now that’ll be the end of it. I will not look for you, I will not pursue you, but if you don’t, I will look for you, I will find you and I will kill you.",
   };
 
   const handleMessaging = () => {
     // Handle messaging functionality here
     // For demonstration, you can navigate to a messaging screen
-    navigation.navigate('MessagingScreen');
+    navigation.navigate("Message");
   };
 
   return (
@@ -33,12 +34,12 @@ const RequestListing: React.FC<RequestListingProps> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
   },
   name: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   description: {
@@ -46,16 +47,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: "#007bff",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
