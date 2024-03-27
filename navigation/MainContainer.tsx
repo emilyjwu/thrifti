f18d328c6ae7848e6a1faed6834803ba5ff5715d
IshaPerry <ishaperry@gmail.com>
2024-03-26 21:57:33 -0400

    fix explore name conflict

--- navigation/MainContainer.tsx
+++ navigation/MainContainer.tsx
@@ -37,7 +37,7 @@ const Stack = createStackNavigator();
 const ExploreStack = ({ navigation }) => (
   <Stack.Navigator>
     <Stack.Screen
-      name="Explore"
+      name="ExploreFeed"
       component={ExploreScreen}
       options={{ headerShown: false }}
     />
