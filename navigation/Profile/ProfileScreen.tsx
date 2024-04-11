7d55e3a4dd18bb59532ce1b86c66a83a32f014e2
Wu, Emily Jaling <ewu64@gatech.edu>
2024-04-11 09:13:30 -0400

    Top part of profile page

--- navigation/Profile/ProfileScreen.tsx
+++ navigation/Profile/ProfileScreen.tsx
@@ -1,30 +1,134 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
-import ListingScroll from '../../components/ListingScroll';
-import { ScrollView } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 
 interface ProfileScreenProps {
   navigation: NavigationProp<any>;
 }
 
+const screenWidth = Dimensions.get('window').width;
+const profilePhotoSize = screenWidth * 0.3; 
+const followButtonWidth = screenWidth * 0.5;
+
 const ProfileScreen: React.FC<ProfileScreenProps> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text
-        onPress={() => navigation.navigate('Explore')}
-        style={{ fontSize: 26, fontWeight: 'bold' }}>Profile Screen</Text>
+      {/* This header should only appear if you navigate to Profile from ANOTHER page */}
+      <View style={styles.header}>
+          <MaterialIcons name="keyboard-arrow-left" size={30} />
+          <View style={styles.usernameContainer}>
+            <Text>janedoe123</Text>
+          </View>
+        </View>
+      <View style={{paddingBottom: 10, paddingHorizontal: 10}}>
+        <View style={styles.topContainer}>
+          <FontAwesome name="user-circle" size={profilePhotoSize} color='gray' style={styles.profilePhoto}/>
+          <View style={styles.verticalColumn}>
+            <Text style={styles.nameText}>Jane Doe</Text>
+            <View style={styles.horizontalRow}>
+              <TouchableOpacity style={styles.followButton} onPress={()=>console.log("following")}>
+                <View>
+                  <Text style={styles.followText}>Follow</Text>
+                </View>
+              </TouchableOpacity>
+              <Feather name="mail" size={35}/>
+            </View>
+          </View>
+        </View>
+        {/* Write this in a loop eventually */}
+        <View style={styles.statsContainer}>
+          <View style={[styles.verticalColumn, {alignItems: 'center'}]}>
+            <Text style={styles.statsNumber}>5</Text>
+            <Text>Sold</Text>
+          </View>
+          <View style={[styles.verticalColumn, {alignItems: 'center'}]}>
+            <Text style={styles.statsNumber}>8</Text>
+            <Text>Purchased</Text>
+          </View>
+          <View style={[styles.verticalColumn, {alignItems: 'center'}]}>
+            <Text style={styles.statsNumber}>25</Text>
+            <Text>Followers</Text>
+          </View>
+          <View style={[styles.verticalColumn, {alignItems: 'center'}]}>
+            <Text style={styles.statsNumber}>24</Text>
+            <Text>Following</Text>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.bioText}>
+            Hi I'm Jane! Feel free to message me about anything you're interested in.
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+  },
+  usernameContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profilePhoto: {
+    height: profilePhotoSize,
+    width: profilePhotoSize,
+    marginRight: 10,
+  },
+  verticalColumn: {
+    flexDirection: 'column',
+  },
+  nameText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
+  horizontalRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  followButton: {
+    backgroundColor: 'lightblue',
+    height: 35,
+    width: followButtonWidth,
+    borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    marginRight: 5,
+  },
+  followText: {
+    fontSize: 17,
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  statsNumber: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bioText: {
+    fontSize: 15,
+    marginTop: 15,
+    marginBottom: 15,
   },
 });
 
